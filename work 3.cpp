f23445c3f551cb4a429092774c6e25d7f8939324
ZdogDio <52146637+ZdogDio@users.noreply.github.com>
2019-07-02 23:47:27 +0700

    Add files via upload

---
+++ work 3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int TH;
+	cout << "Input Thai Baths : ";
+	cin >> TH;
+	cout << TH <<"baths is "<< TH*0.033 << "dollar " << endl ;
+	cout << TH <<"baths is "<< TH*0.029 << "euro " << endl ;
+}
